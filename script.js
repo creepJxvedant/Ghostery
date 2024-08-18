4824d801d5474a4f0bc51f7363c671a3df728ef4
creepJxvedant <vedantverma303@gmail.com>
2024-08-18 20:32:36 +0530

    updated 2

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 
 
 
-//   const apiKey = `b402078b6c7440a6a78b64cb8131697a`;
-//   const url1=`https://api.rawg.io/api/genres?key=${apiKey}`;
-//   const url = `https://api.rawg.io/api/games?key=${apiKey}`;
-//   const todaydate=`2024-16-08`;
-//   const todaysURL=`https://api.rawg.io/api/games?key=${apiKey}&page_size=6&dates=2020-12-12,2024-01-01`;
+  const apiKey = `b402078b6c7440a6a78b64cb8131697a`;
+  const url1=`https://api.rawg.io/api/genres?key=${apiKey}`;
+  const url = `https://api.rawg.io/api/games?key=${apiKey}`;
+  const todaydate=`2024-16-08`;
+  const todaysURL=`https://api.rawg.io/api/games?key=${apiKey}&page_size=6&dates=2020-12-12,2024-01-01`;
 
 
 
@@ -68,71 +68,69 @@
 
 
 
-//   // Fetch data from RAWG API
-//   fetch(url)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! Status: ${response.status}`);
-//       }
-//       return response.json();
-//     })
-//     .then(data => showData(data))
-//     .catch(error => {
-//       console.error('Error fetching data:', error);
-//     });
+ // Fetch data from RAWG API
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => showData(data))
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
   
-//   const wrapper = document.querySelector(".local-list-row");
+  const wrapper = document.querySelector(".local-list-container .games-container");
   
-//   function showData(data) {
-//     data.results.forEach(game => {
-//             const gameCard = document.createElement("div");
+  function showData(data) {
+    data.results.forEach(game => {
+            const gameCard = document.createElement("div");
             
-//             const gameinfo=document.createElement("div");
-//             const span1=document.createElement("span");
-//             span1.className="gamename"
-//             const span2=document.createElement("span");
-//             const span3=document.createElement("span");
-//             span1.textContent=game.name;
-//             span2.textContent = `rating: ${(Math.round(game.rating * 200) / 100).toFixed(1)}`;
-//             span3.textContent=`Genre: ${game.genres[0].name} ${game.genres[1]?`| ${game.genres[1].name}`:''}`;
-//             const starImg = document.createElement("img");
-//             starImg.src = "assests/star.svg";
-//             starImg.alt = "Star";
-//             starImg.className = "star-icon";
-//             span2.appendChild(starImg);
-//             gameinfo.appendChild(span1);
-//             gameinfo.appendChild(span2);
-//             gameinfo.appendChild(span3);
+            const gameinfo=document.createElement("div");
+            const span1=document.createElement("span");
+            span1.className="gamename"
+            const span2=document.createElement("span");
+            const span3=document.createElement("span");
+            span1.textContent=game.name;
+            span2.textContent = `rating: ${(Math.round(game.rating * 200) / 100).toFixed(1)}`;
+            span3.textContent=`Genre: ${game.genres[0].name} ${game.genres[1]?`| ${game.genres[1].name}`:''}`;
+            const starImg = document.createElement("img");
+            starImg.src = "assests/star.svg";
+            starImg.alt = "Star";
+            starImg.className = "star-icon";
+            span2.appendChild(starImg);
+            gameinfo.appendChild(span1);
+            gameinfo.appendChild(span2);
+            gameinfo.appendChild(span3);
 
-//             gameCard.className = "local-list-card";
-//             gameinfo.className="gameinfo";
-//             gameCard.appendChild(gameinfo);
+            gameCard.className = "game";
+            gameinfo.className="gameinfo";
+            gameCard.appendChild(gameinfo);
 
-//             gameCard.style.backgroundImage = `url(${game.background_image})`;
-//             wrapper.appendChild(gameCard);
-//           });
-//   }
+            gameCard.style.backgroundImage = `url(${game.background_image})`;
+            wrapper.appendChild(gameCard);
+          });
+  }
   
 
-//  let lastindex=-1;
-//   const sidebargenre=document.querySelectorAll(".local-sidebar ul li");
-//   sidebargenre.forEach(side => {
-//     side.addEventListener("click", () => {
-//         if(lastindex!=-1){
-//             sidebargenre[lastindex].classList.remove("genre-active");
-//         }
-//         side.classList.add("genre-active");
-//         lastindex=side.getAttribute('index');
-//     })
-//   });
+ let lastindex=-1;
+  const sidebargenre=document.querySelectorAll(".local-sidebar ul li");
+  sidebargenre.forEach(side => {
+    side.addEventListener("click", () => {
+        if(lastindex!=-1){
+            sidebargenre[lastindex].classList.remove("genre-active");
+        }
+        side.classList.add("genre-active");
+        lastindex=side.getAttribute('index');
+    })
+  });
 
 
 
 
   const btn=document.querySelector(".genre-toggler");
   const genresidebar=document.querySelector(".local-sidebar");
-  btn.classList.add("active");
-  genresidebar.classList.add("active");
 
 
   btn.addEventListener("click", () => {
